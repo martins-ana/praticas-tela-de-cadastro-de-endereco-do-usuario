626bea7ced60e0a17d6a985fc6cdd3daf715caef
Ana Paula <martinsgarciaana@outlook.com>
2025-04-09 11:58:00 -0300

    style: aplicado estilos ao formulário

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
 function App() {
   return (
-    <section>
-      <h1>Cadastro de Endereço</h1>
-      <form>
+    <section className='gradiente-primario flex flex-col items-center justify-center h-screen w-screen px-16 py-64 gap-16'>
+      <h1 className='font-bold text-5xl'>Cadastro de Endereço</h1>
+      <form className=' flex flex-col gap-16 items-center w-full h-full'>
         <div>
           <fieldset>
-            <label htmlFor='cep'>CEP:</label>
+            <label htmlFor='cep'>CEP*:</label>
             <input type='text' id='cep' name='cep' />
           </fieldset>
           <fieldset>
-            <label htmlFor='estado'>Estado:</label>
+            <label htmlFor='estado'>Estado*:</label>
             <input type='text' id='estado' name='estado' />
           </fieldset>
           <fieldset>
-            <label htmlFor='cidade'>Cidade:</label>
+            <label htmlFor='cidade'>Cidade*:</label>
             <input type='text' id='cidade' name='cidade' />
           </fieldset>
           <fieldset>
-            <label htmlFor='bairro'>Bairro:</label>
+            <label htmlFor='bairro'>Bairro*:</label>
             <input type='text' id='bairro' name='bairro' />
           </fieldset>
           <fieldset>
-            <label htmlFor='rua'>Rua:</label>
+            <label htmlFor='rua'>Rua*:</label>
             <input type='text' id='rua' name='rua' />
           </fieldset>
           <fieldset>
-            <label htmlFor='numero'>Número:</label>
+            <label htmlFor='numero'>Número*:</label>
             <input type='text' id='numero' name='numero' />
           </fieldset>
           <fieldset>
@@ -37,21 +37,27 @@ function App() {
             <input type='text' id='referencia' name='referencia' />
           </fieldset>
         </div>
-        <div>
-          <input
-            type='checkbox'
-            id='aceitarTermosCadastro'
-            name='aceitarTermosCadastro'
-          />
-          <label htmlFor='aceitarTermosCadastro'>Aceito os termos de cadastro</label>
-          <span>
+
+        <fieldset className='flex flex-col items-start'>
+          <div className='flex gap-2'>
+            <input
+              type='checkbox'
+              id='aceitarTermosCadastro'
+              name='aceitarTermosCadastro'
+            />
+            <label htmlFor='aceitarTermosCadastro'>Aceito os termos de cadastro</label>
+          </div>
+          <span className='ml-5 text-dica text-sm'>
             É necessário aceitar os termos e condições para concluir o cadastro.
           </span>
-        </div>
+        </fieldset>
 
-        <div>
-          <button type='submit'>Enviar</button>
-        </div>
+        <button
+          className='bg-primaria rounded-[8px] flex items-center justify-center p-3 gap-2'
+          type='submit'
+        >
+          Salvar
+        </button>
       </form>
     </section>
   );
